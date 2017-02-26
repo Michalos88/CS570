353257f482699ef7d578dde5fb35842a0298a8ad
Michal Lyskawinski <mscteam99@gmail.com>
2017-02-25 22:55:07 -0500

    Update and rename Lab2 - Caesar Cipher.js to Caesar Cipher.js

--- Caesar Cipher.js
+++ Caesar Cipher.js
@@ -1,12 +1,7 @@
 /**
  * Created by Michalos88 on 2/9/2017.
  */
-// You will be deciphering a given message. It is encrypted with a Caesar Cipher (https://en.wikipedia.org/wiki/Caesar_cipher)
-// that increases by 2 after every 3 characters (including symbol characters, which are not encoded), starting at key = 5.
-// You should save this message to a file using a text editor.
-// Then, your program will prompt the user for the name of the file,
-// decrypt the message stored in the file,
-// and then write the decrypted message to a new file called solution.txt.
+
 
 //Importing
 var fs = require("fs");
